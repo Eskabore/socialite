4abff12efb380c64e12b530da8537e50c35041a6
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-03-12 21:26:55 +0100

    Refactored code

--- client/components/Profile.jsx
+++ client/components/Profile.jsx
@@ -3,26 +3,36 @@ import BioEditor from './BioEditor';
 
 export default function Profile(props) {
 
+    const { first, last, picture, bio, toggleUploader, bioUpdated } = props;
+
+    // Add error handling for missing or invalid props
+    if (!first || !last || !picture) {
+        return <div>Error: missing props</div>;
+    }
+
+    // Check that picture is a valid URL
+    const pictureUrl = new URL(picture);
+    if (!pictureUrl.protocol.startsWith('http')) {
+        return <div>Error: Invalid picture URL</div>;
+    }
 
     return (
-        <div>
-            <div className='profile'>
-                <ProfilePic
-                    first={props.first}
-                    last={ props.last }
-                    picture={ props.picture }
-                    toggleUploader={ props.toggleUploader }
-                    size="large"
-                />
+        <section className='profile'>
+            <ProfilePic
+                first={first}
+                last={last}
+                picture={picture}
+                toggleUploader={toggleUploader}
+                size="large"
+            />
 
-                <div className="user-info">
-                    <h4>
+            <div className="user-info">
+                <h2>Name:</h2>
+                <p>{first} {last}</p>
 
-                      Name:  { props.first } <br /> Last Name: { props.last }
-                    </h4>
-                    <BioEditor bio={ props.bio } bioUpdated={ props.bioUpdated} />
-                </div>
+                <h2>Bio:</h2>
+                <BioEditor bio={bio} bioUpdated={bioUpdated} />
             </div>
-        </div>
+        </section>
     );
 }

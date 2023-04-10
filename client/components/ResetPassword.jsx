b1f289018d10a66b155bec6aff2ced709927faf0
Jean-Luc KABORE-TURQUIN <turquinjl@gmail.com>
2023-04-10 12:50:07 +0200

    [WIP] Registration component from class to functional

--- client/components/ResetPassword.jsx
+++ client/components/ResetPassword.jsx
@@ -1,159 +1,116 @@
-import { Component } from "react";
+import { useState } from "react";
 import { Link } from "react-router-dom";
-export default class ResetPassword extends Component {
-    constructor(props) {
-        super(props);
 
-        this.state = {
-            email: "",
-            code: "",
-            password: "", // Updated password
-            error: false,
-            step: 1 // Component first mount
-        };
+const ResetPassword = () => {
+    const [email, setEmail] = useState("");
+    const [code, setCode] = useState("");
+    const [password, setPassword] = useState("");
+    const [error, setError] = useState(false);
+    const [step, setStep] = useState(1);
 
-        this.handleChange = this.handleChange.bind(this);
-        this.handleSubmit = this.handleSubmit.bind(this);
-        this.handleVerify = this.handleVerify.bind(this);
-    }
+    const handleChange = (e) => {
+        const { name, value } = e.target;
 
-    handleChange(e) {
-        console.log(e);
-        this.setState(
-            {
-                [e.currentTarget.name]: e.currentTarget.value,
-            },
-            () => {
-                console.log(this.state);
+        if (name === "email") {
+            setEmail(value);
+        } else if (name === "code") {
+            setCode(value);
+        } else if (name === "password") {
+            setPassword(value);
+        }
+    };
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        const rpEmail = { email };
+
+        try {
+            const response = await fetch("/password/reset/start.json", {
+                method: "POST",
+                body: JSON.stringify(rpEmail),
+                headers: { "Content-Type": "application/json" },
+            });
+
+            if (response.ok) {
+                setStep(2);
+            } else {
+                setError(true);
             }
+        } catch {
+            setError(true);
+        }
+    };
+
+    const handleVerify = async (e) => {
+        e.preventDefault();
+
+        const rpCode = { email, code, password };
+
+        try {
+            const response = await fetch("/password/reset/verify.json", {
+                method: "POST",
+                body: JSON.stringify(rpCode),
+                headers: { "Content-Type": "application/json" },
+            });
+
+            if (response.ok) {
+                setStep(3);
+            } else {
+                setError(true);
+            }
+        } catch {
+            setError(true);
+        }
+    };
+
+    if (step === 1) {
+        return (
+            <div>
+                <form onSubmit={handleSubmit}>
+                    <input
+                        type="email"
+                        name="email"
+                        placeholder="email"
+                        value={email}
+                        onChange={handleChange}
+                    />
+                    <button type="submit">Next Step</button>
+                </form>
+            </div>
+        );
+    } else if (step === 2) {
+        return (
+            <div>
+                <form onSubmit={handleVerify}>
+                    <input
+                        type="text"
+                        name="code"
+                        placeholder="code"
+                        value={code}
+                        onChange={handleChange}
+                    />
+                    <input
+                        type="password"
+                        name="password"
+                        placeholder="New Password"
+                        value={password}
+                        onChange={handleChange}
+                    />
+                    <button type="submit">Set New Password</button>
+                </form>
+            </div>
+        );
+    } else {
+        return (
+            <div>
+                <h1>Password successfully Updated</h1>
+                <button>
+                    <Link to="/login">Log In</Link>
+                </button>
+            </div>
         );
     }
+};
 
-    handleSubmit(e) {
-        e.preventDefault();
-
-        console.log(this.state.step);
-
-        const rpEmail = {};
-
-        rpEmail.email = this.state.email;
-        console.log(rpEmail);
-
-        fetch('/password/reset/start.json', {
-            method: 'POST',
-            body: JSON.stringify(rpEmail),
-            headers: { 'Content-Type': 'application/json'}
-        })
-            .then((response) => {
-                console.log("response is: ", response.ok);
-
-                if(response.ok) {
-
-                    // User should have received an email with a random code
-                    // NXT (Step 2) is verify code
-                    this.setState( { step: 2 }); // Step 2
-                    console.log("Fetch 'reset/start.json' succeeded");
-                } else {
-                    this.setState( { error: true }); //
-                }
-            });
-    }
-
-    handleVerify(e) {
-        e.preventDefault();
-
-        console.log(this.state.step);
-
-        const rpCode = {};
-
-        rpCode.email = this.state.email;
-        rpCode.code = this.state.code;
-        rpCode.password = this.state.password;
-
-        console.log(rpCode);
-
-        fetch("/password/reset/verify.json", {
-            method: "POST",
-            body: JSON.stringify(rpCode),
-            headers: { 'Content-Type': 'application/json' }
-        })
-            .then((response) => {
-                console.log("response is: ", response.ok);
-
-                if (response.ok) {
-                // Code should match and User set new password
-                // NXT (Step 3) OR 'else'
-                    this.setState({ step: 3 });
-                    console.log("Fetch 'reset/verify.json' succeeded");
-                } else {
-                    this.setState({ error: true });
-                }
-            });
-    }
-
-    render () {
-        if (this.state.step === 1) {
-            // Render form with email field
-            // and button next Step
-            return (
-                <div>
-
-                    <form onSubmit={this.handleSubmit}>
-                        <input
-                            type="email"
-                            name="email"
-                            placeholder="email"
-                            value={this.state.email}
-                            onChange={this.handleChange}
-                        />
-                        <button type="submit">
-                            Next Step
-                        </button>
-                    </form>
-
-                </div>
-            );
-        } else if (this.state.step === 2) {
-            // Verify Code
-            // Field for New Password
-            return (
-                <div>
-
-                    <form onSubmit={this.handleVerify}>
-                        <input
-                            type="text"
-                            name="code"
-                            placeholder="code"
-                            value={this.state.code}
-                            onChange={this.handleChange}
-                        />
-
-                        <input
-                            type="password"
-                            name="password"
-                            placeholder="New Password"
-                            value={this.state.password}
-                            onChange={this.handleChange}
-                        />
-
-                        <button type="submit">
-                            Set New Password
-                        </button>
-                    </form>
-                </div>
-            );
-        } else {
-            // Step 3
-            return (
-                <div>
-                    <h1>Password successfully Updated</h1>
-                    <button>
-                        <Link to='/login'>
-                            Log In
-                        </Link>
-                    </button>
-                </div>
-            );
-        }
-    }
-}
+export default ResetPassword;
